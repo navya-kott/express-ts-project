595635802c5a74f1ec5c11c3b072523d68015035
navya <navya.ak@kottsoftware.com>
2025-02-20 09:03:49 +0530

    car list service updated

--- src/car/service/car-list.ts
+++ src/car/service/car-list.ts
@@ -3,18 +3,23 @@ import { Car } from "@/schema/car"
 export const carList = async () => {
   try {
     return await Car.aggregate([
-      { $match: { brand: 'Chevrolet' } },
-      // {
-      //   $group:
-      //     {
-      //       _id:"$brand",
-      //       model: { $addTSet: "$model" }
-      //     }
-      // },
-      { $sort: { brand: 1 } }
+      {
+        $group: {
+          _id: "$brand",
+          model: { $first: "$model" },
+          year: { $first: "$year" }    
+        }
+      },
+      {
+        $project: {
+          _id: 1,
+          model: 1,
+          year: 1
+        }
+      },
+      { $sort: { year: -1 } }
     ])
-    // return await Car.find({brand:'Chevrolet' })
   } catch (error) {
     return new Error('Car listing failed')
   }
-}
+};
